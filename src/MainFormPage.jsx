666a4c7e384f69de196ee669726dd522645a94d8
Silma Thoron <paulgivel@gmail.com>
2025-06-11 13:30:19 +0200

    feat: replace Logout link with button to trigger logout function

--- src/MainFormPage.jsx
+++ src/MainFormPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useAppContext } from "./AppContext";
 import App from "./App";
 import CenteredCardLayout from "./components/CenteredCardLayout";
 
 export default function MainFormPage() {
+  const { logout } = useAppContext();
   return (
     <CenteredCardLayout>
       <div className="flex justify-between mb-4">
         <span />
-        <Link to="/login" className="text-sky-600 underline">Logout</Link>
+        <button
+          className="text-sky-600 underline bg-transparent border-none cursor-pointer p-0"
+          onClick={logout}
+        >
+          Logout
+        </button>
       </div>
       <App />
     </CenteredCardLayout>
